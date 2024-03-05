2a2ea9c635033039fe2cd231be274b27a64eb28c
K4 - Tuan Do <hoangtuan176@gmail.com>
2024-03-06 00:17:55 +0700

    init

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.guru99.com/xpath-selenium.html');
+  // await page.waitForTimeout(10000);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/XPath in Selenium/);
 });
-
+//test started link
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.guru99.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole('link', { name: '➤ Selenium' }).click();
+  // await page.getByRole('link', { name: 'Selenium' }).first().click();
+  
+  // Expects page to have a heading with the name of Selenium Tutorial Syllabus.
+  await expect(page.getByRole('heading', { name: 'Selenium Tutorial Syllabus' })).toBeVisible();
 });
